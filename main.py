538f0abba715befdb8496c674567a656b38f3b5f
masihtabaei <masihtabaei@gmail.com>
2025-02-14 09:46:39 +0100

    Continues #1 and #2
    Added a simple lifespan to control the startup and shutdown logic and
    got rid of one of dummy endpoints

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from typing import Union
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Loaded!")
+    yield
+    print("Unloaded!")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app = FastAPI(lifespan=lifespan)
