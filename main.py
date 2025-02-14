a5c4a03a66e1c7795e6136fb58b8f4e41f11a893
masihtabaei <masihtabaei@gmail.com>
2025-02-14 10:23:57 +0100

    Continues #2
    Added a clean yet simple logger configuration to the lifespan and also
    logged startup and shutdown completeion messages using the logger
    instead of using two print statements

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from contextlib import asynccontextmanager
+import logging
 from fastapi import FastAPI
+import sys
+
+def configure_logger(app: FastAPI):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    file_handler = logging.FileHandler("info.log")
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+    app.state.logger = logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Loaded!")
+    configure_logger(app)
+    app.state.logger.info("Application startup completed!")
     yield
-    print("Unloaded!")
+    app.state.logger.info("Application shutdown process completed!")
 
 app = FastAPI(lifespan=lifespan)
