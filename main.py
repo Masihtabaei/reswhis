80da98f37034cd8b6e1431743c6a6c6bf4fe3865
masihtabaei <masihtabaei@gmail.com>
2025-02-14 12:11:29 +0100

    Continues #1, #2 and #3
    Added the whisper_online dependency as script, wrote a function to load
    the faster Whisper tiny model and also cleaned-up the logging mechanism

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from contextlib import asynccontextmanager
-import logging
 from fastapi import FastAPI
+import whisper_online
+import logging
 import sys
 
+def load_faster_whisper_tiny_model(app: FastAPI):
+    app.state.whisper_tiny_model = whisper_online.FasterWhisperASR('en', 'tiny')
+    app.state.logger.info('Tiny model loaded successfully!')
+    
 def configure_logger(app: FastAPI):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
+    formatter = logging.Formatter('%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s')
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
-    file_handler = logging.FileHandler("info.log")
+    file_handler = logging.FileHandler('info.log')
     file_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
     app.state.logger = logger
+    app.state.logger.info('Logger configured successfully!')
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     configure_logger(app)
-    app.state.logger.info("Application startup completed!")
+    load_faster_whisper_tiny_model(app)
+
+    app.state.logger.info('Application startup completed!')
     yield
-    app.state.logger.info("Application shutdown process completed!")
+    app.state.logger.info('Application shutdown process completed!')
 
 app = FastAPI(lifespan=lifespan)
