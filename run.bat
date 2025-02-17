de5dd4cf98f40c8892b0ed2b7bd0ecfa3055ddba
masihtabaei <masihtabaei@gmail.com>
2025-02-17 13:53:08 +0100

    Continues #1 and #6
    Corrected the bacth file by using set instead of export

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
-export BACKEDN="faster-whisper"
-export MODEL_SIZE="large"
+set BACKEDN=faster-whisper
+set MODEL_SIZE=large
+set LANGUAGE=en
+set SAMPLING_RATE=16000
+set MINIMUM_CHUNK_SIZE=2.0
 uv run uvicorn main:app --reload
